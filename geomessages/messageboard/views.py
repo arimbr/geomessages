ee2f36201cd9df8f54eabd665713edeb576c0daf
arimbr <arimbr@gmail.com>
2015-04-15 20:25:49 +0300

    Write basic URL and views

--- geomessages/messageboard/views.py
+++ geomessages/messageboard/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+def message_list(request):
+    return HttpResponse(content="Hello World")
