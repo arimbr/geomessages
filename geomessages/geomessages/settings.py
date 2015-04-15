46c085558d92ece8f517cb6abf54ceb75cfc2741
arimbr <arimbr@gmail.com>
2015-04-15 20:52:19 +0300

    Configure PostgreSQL as default database'

--- geomessages/geomessages/settings.py
+++ geomessages/geomessages/settings.py
@@ -75,12 +75,23 @@ WSGI_APPLICATION = 'geomessages.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'messages',
+        'USER': 'ari',
+        'PASSWORD': 'ari',
+        'HOST': 'localhost',
     }
-}
+}   
 
 
 # Internationalization
