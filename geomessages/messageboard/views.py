98bd8c8009c1d4f2111f979bf7aae7190f522662
arimbr <arimbr@gmail.com>
2015-04-27 19:43:35 +0300

    Comment code

--- geomessages/messageboard/views.py
+++ geomessages/messageboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import HttpResponse
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
 
@@ -11,13 +11,14 @@ from .serializers import GeomessageSerializer
 # Create your views here.
 def message_list(request):
     # Get access to URL query parameters
-    # /api/v1/messages/?lat=66.434&lng=44.3453
+    # /api/v1/messages/?lat=66.434&lng=66.3453
     lat = request.GET.get('lat') or 0
     lng = request.GET.get('lng') or 0
     origin = Point(float(lat), float(lng))
 
-    # Find geomessages that are within 1000 km from the user location
-    qs = Geomessage.objects.filter(location__distance_lte=(origin, D(km=1000)))
+    # Find geomessages that are within 50 km from the user location
+    qs = Geomessage.objects.filter(location__distance_lte=(origin, D(km=50)))
+    # TODO: qs stills need to be ordered by distance
     serializer = GeomessageSerializer(qs, many=True)
     json_content = JSONRenderer().render(serializer.data)
 
