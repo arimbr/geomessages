641eeaecab4e9d4cfbea06304e739ad6741d560c
arimbr <arimbr@gmail.com>
2015-05-02 22:04:45 +0300

    Add POST method to message_list view

--- messageboard/views.py
+++ messageboard/views.py
@@ -1,6 +1,7 @@
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
 
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -8,11 +9,16 @@ from .models import Geomessage
 from .serializers import GeomessageSerializer
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def message_list(request):
     """
     List all messages, or create a new message
     GET /api/v1/messages/?lat=66.434&lng=66.3453
+    POST content
+    {
+        "message": "I wish Finnish people would gather outdoors together more often, like in Vappu",
+        "location": { "type": "Point", "coordinates": [ 60.1708, 24.9375 ] }
+    }
     """
     if request.method == 'GET':
         # Get URL query parameters
@@ -27,3 +33,10 @@ def message_list(request):
         serializer = GeomessageSerializer(messages, many=True)
 
         return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = GeomessageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
