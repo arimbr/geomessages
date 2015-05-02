cb39d1a1567db75d2efe49878957484ed3dd1277
arimbr <arimbr@gmail.com>
2015-05-02 21:49:35 +0300

    Configure GeoManager in model for geographic queries

--- messageboard/models.py
+++ messageboard/models.py
@@ -6,3 +6,7 @@ class Geomessage(models.Model):
     message = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
     location = models.PointField()
+
+    # In order to conduct geographic queries,
+    # each geographic model requires a GeoManager model manager.
+    objects = models.GeoManager()
